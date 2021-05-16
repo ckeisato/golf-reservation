362238abb17f98ce94115c16f0e7d5e05f4f7d45
Kei Sato <ckeisato@gmail.com>
2021-05-15 19:46:48 -0700

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # golf-reservation
+todo:
+- cleanup: change selectors to use xpath to search for visible text
+- cleanup: date picker selector?
+
+
+7pm booker:
+- GCP function, at 6:59:500 executes function
+- checks availability
+- given hour, books first option within the hour
+
+
+schedule polling:
+- runs every 10 min
+- checks for available times 
+    - each day
+    - each course
+- sends availablity by text/email
--- index.js
+++ index.js
@@ -11,45 +11,63 @@ const screen = {
   height: 900
 };
 
+const sleep = 700;
+const date = "05-22-2021";
+
 (async function myFunction() {
     // headless
-    let driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(new chrome.Options().headless().windowSize(screen))
-      .build();
-
-    // not headless
     // let driver = await new Builder()
     //   .forBrowser('chrome')
+    //   .setChromeOptions(new chrome.Options().headless().windowSize(screen))
     //   .build();
 
+    // not headless
+    const driver = await new Builder()
+      .forBrowser('chrome')
+      .build();
 
+
+    // navigate main page
     await driver.get('https://foreupsoftware.com/index.php/booking/19765/2431#teetimes');
     const bookingClasses = await driver.findElement(By.className("booking-classes"));
     const resident = await bookingClasses.findElement(By.className("btn-primary"));
     await resident.click();
-
-
     const login = await driver.findElement(By.className("login"));
     await login.click();
+
+    // login modal
     await driver.wait(until.elementLocated(By.id("login"), 100));
-
-
     const email = await driver.findElement(By.id("login_email"));
     const password = await driver.findElement(By.id("login_password"));
 
+
+    // enter credentials
     email.sendKeys("ckeisato@gmail.com");
     password.sendKeys("golfgolf");
 
     const modal = await driver.findElement(By.id("login"));
     const loginButton = await modal.findElement(By.className("login"));
 
-
-    console.log(1);
-    await driver.sleep(600);
-
-
+    // recapta
+    await driver.sleep(sleep);
+    console.log(0);
     await loginButton.click();
+
+    // navigate to reservations
+    await driver.sleep(sleep);
+    const reservations = await driver.findElement(By.xpath("//*[@href='#/teetimes']"));
+    await reservations.click();
+  
+    // await driver.wait(until.elementLocated(By.id("date-field"), 100));
+    // const datePicker = await driver.findElement(By.id("date-field"));
+
+    // // await datePicker.clear();  
+    // datePicker.sendKeys("value", date);
+
+    const temp = await driver.findElement(By.xpath("//td[contains(text(),'22')] "));
+    console.log(temp);
+    temp.click();
+
 
     console.log(2);
 
